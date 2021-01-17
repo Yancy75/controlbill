252836e3ced6f5d4a5324272848504f737d5ed84
Yancy Marine <yanci.marine@gmail.com>
2021-01-17 12:32:51 -0400

    activo

--- resources/views/payroll/dashboard.blade.php
+++ resources/views/payroll/dashboard.blade.php
@@ -34,10 +34,14 @@
                                 <h5 class="th3">Select one of the supermarket to start work</h5>
                             @endif
                             <div class="row" style="justify-content: center;">
-                                @foreach($info as $inf)
+                                @foreach($info as $inf)                                
                                     <div class="animated flipInY marco " data-toggle="tooltip" data-placement="bottom" title="{{ $inf['name'] }}">
                                         <a href="{{ route('select_supermarket', ['id' => $inf['id']]) }}" >
-                                            <div class="tile-stats by">
+                                            @if($inf['supermarket_id'] == session()->get('supermarket_id'))
+                                            <div class="tile-stats by activomark">
+                                             @else
+                                             <div class="tile-stats by">
+                                             @endif   
                                               <div class="tile1">
                                                 <div class="count">{{ $inf['name'] }}</div>
                                                 <div class="tile2"><i class="fa fa-shopping-cart"></i></div>
